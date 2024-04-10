6254ecc4e52eb79b2348bbbced2ec1cf4f14014f
crrenatacr <crrenatacr@gmail.com>
2024-04-10 17:08:08 +0200

    JQuerySecondTrial

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
-
-// scripts.js
-
 function newItem() {
-    // Adding a new item to the list of items
+    // Adding a new item to the list of items:
     let inputValue = $('#input').val();
-
     if (inputValue === '') {
         alert("You must write something!");
     } else {
         let li = $('<li></li>').text(inputValue);
         $('#list').append(li);
-        $('#input').val(''); // Clear the input field after adding
-
-        // Crossing out an item from the list of items
-        li.on('dblclick', function() {
-            $(this).toggleClass("strike");
-        });
-
-        // Adding the delete button "X"
-        let crossOutButton = $('<button>X</button>');
-        li.append(crossOutButton);
-
-        crossOutButton.on('click', function() {
-            $(this).parent().addClass("delete");
-        });
     }
 
-    // Reordering the items
+    // Crossing out an item from the list of items:
+    function crossOut() {
+        $(this).toggleClass("strike");
+    }
+
+    $('#list').on("dblclick", "li", crossOut);
+
+    // Adding the delete button "X":
+    let crossOutButton = $('<button>X</button>');
+    let li = $('<li></li>').append(inputValue).append(crossOutButton);
+    crossOutButton.on("click", deleteListItem);
+
+    // Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        $(this).parent().addClass("delete");
+    }
+
+    // Reordering the items:
     $('#list').sortable();
 }
